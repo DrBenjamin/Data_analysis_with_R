533d4e512667d1baf82838ea1255d9fd22830fac
Benjamin Gross <ben@benbox.org>
2024-01-08 09:56:39 +0100

    added example code

--- r-scripts/Week 1.R
+++ r-scripts/Week 1.R
@@ -4,4 +4,9 @@
 pacman::p_load(tidyverse)
 
 ## Loading data
-
+# Piped functions
+esoph %>%
+  filter(agegp == "65-74") %>%     # only keep rows where age group is 65 to 74
+  filter(alcgp == "0-39g/day") %>% # only keep rows where alcohol consumption is 0 to 39 g per day
+  select(-agegp) %>%               # remove the age group variable
+  select(-alcgp)                   # remove the alcohol consumption variable

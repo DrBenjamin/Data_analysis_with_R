5c0331ccbf5a81dcc52662130d70bdc37a22c5e1
Benjamin Gross <ben@benbox.org>
2024-01-16 10:19:10 +0100

    added Week 2 code

--- r-scripts/Week 2.R
+++ r-scripts/Week 2.R
@@ -1,4 +1,8 @@
 ### Course "Data analysis with R"
 ### Week 2
 ## Libraries
-pacman::p_load(tidyverse, NHANES)
+pacman::p_load(tidyverse, here, NHANES)
+
+
+## Loading data
+data <- read_csv(here("raw_data", "walking_clinic_January.csv"))

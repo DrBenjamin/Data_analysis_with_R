97065ebd78e4bc06ddd0716ae3c8f634393b70a9
Benjamin Gross <ben@benbox.org>
2024-01-16 11:02:30 +0100

    minor improvements

--- r-scripts/Week 2_Uploading_Eyeballing.R
+++ r-scripts/Week 2_Uploading_Eyeballing.R
@@ -1,13 +1,12 @@
 ## Script for loading in and eyeballing data
-
-## Load required libraries
+# Load required libraries
 library(tidyverse)
 
-## Reading in the data
+# Reading in the data
 data <- read_csv("walkin_clinic_January.csv")
 
-## Eyeballing data
+# Eyeballing data
 View(data)
-head(data, n=10)
+head(data, n = 10)
 str(data)
 glimpse(data)

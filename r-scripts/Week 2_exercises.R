df5ec8704f6fbf5dc7dea611d26b68a44b3653da
Benjamin Gross <ben@benbox.org>
2024-01-22 14:46:15 +0100

    added confidence intervals for proportions

--- r-scripts/Week 2_exercises.R
+++ r-scripts/Week 2_exercises.R
@@ -15,9 +15,9 @@ pacman::p_load(tidyverse, epitools, finalfit, gtsummary, here, broom, NHANES)
 ## with good general health status.
 # Remove missing data and filter data
 cont_tab <- NHANES %>%
-  drop_na(Work,Education,AgeDecade,HealthGen,BMI_WHO) %>%
+  drop_na(Work, Education, AgeDecade, HealthGen, BMI_WHO) %>%
   filter(str_replace_all(AgeDecade, " ", "") == "40-49", HealthGen == "Good", BMI_WHO == "18.5_to_24.9") %>%
-  select(Work,Education)
+  select(Work, Education)
 
 # Show table
 cont_tab %>%
@@ -26,12 +26,27 @@ cont_tab %>%
 
 ## 2. Test the association between Education and Work using both 
 ## Fisher's exact test and a Chi-square test.
+cont_tab %>%
+  table() %>%
+  fisher.test()
 
+# Chi-square test
+cont_tab %>%
+  table() %>%
+  chisq.test() #%>%
+  .$expected
 
 
 ## 3. Test the association between the Education and Work variables 
 ## without filtering the dataset first, using the Chi-square test.
+cont_tab_unfiltered <- NHANES %>%
+  drop_na(Work, Education) %>%
+  select(Work, Education)
 
+# Chi-square test
+cont_tab_unfiltered %>%
+  table() %>%
+  chisq.test()
 
 
 
@@ -47,7 +62,8 @@ cont_tab2 <- c(18, 157, 5, 12) %>%
 
 ## 2. Calculate the relative risk and its confidence interval.
 cont_tab2 %>%
-  epitab(., method = "riskratio", rev = "both")
+  epitab(., method = "riskratio", rev = "both") # No (exposure) and No (outcome)
+                                                # need to be first row & column
 # Confidence (lower, upper) is more important than p-value
 
 
@@ -62,7 +78,7 @@ cont_tab3 <- c(152, 248, 17, 103) %>%
   matrix(., byrow = TRUE, nrow = 2)
   
 cont_tab3 %>%
-  epitab(., method = "oddsratio", rev = "both")
+  epitab(., method = "oddsratio") # As it is a symmetric test, no need for `rev`
 
 
 
@@ -72,11 +88,46 @@ cont_tab3 %>%
 ## Using the NHANES dataset:
 ## 1. Calculate the CI for the proportion of each combination of Diabetes 
 ## and HealthGen categories
+ NHANES %>%
+  select(Diabetes, HealthGen) %>%
+  drop_na() %>%
+  table()
 
+cont_tab4 <- NHANES %>%
+  drop_na(Diabetes, HealthGen) %>%
+  group_by(Diabetes) %>%
+  count(HealthGen) %>%
+  mutate(Total = sum(n)) %>%
+  rowwise %>%
+  mutate(prop = prop.test(n, Total, conf.level = 0.95)$estimate,
+         lower_ci = prop.test(n, Total, conf.level = 0.95)$conf.int[1],
+         upper_ci = prop.test(n, Total, conf.level = 0.95)$conf.int[2])
+
+cont_tab4
 
 ## 2. Calculate the CI for the difference in proportions of individuals 
 ## with very good health between individuals with diabetes and individuals 
 ## without diabetes.
+NHANES %>%
+  drop_na(Diabetes, HealthGen) %>%  # Remove rows with missing Diabetes or HealthGen value
+  group_by(Diabetes) %>%            # Group by Diabetes status (Yes or No), so the counts are calculated separately for each category
+  count(HealthGen) %>%              # Count the number of rows for each combination of variable values (e.g. No diabetes and Excellent health, No diabetes and Vgood health, etc.)
+  mutate(Total = sum(n)) %>%       # Create a new column named Total with the total number of observations for each Diabetes category
+  filter(HealthGen == "Vgood") %>% # Only keep rows where the value of HealthGen is Vgood
+  ungroup() %>%                    # Ungroup the tibble, so it "forgets" about the other values of HealthGen
+  mutate(hasDiabetes = Diabetes == "Yes") %>% # Create a new logical column named hasDiabetes which is TRUE if Diabetes is "Yes" and FALSE otherwise
+  summarise(prop_diabetes = n[hasDiabetes]/Total[hasDiabetes], # Create a new column called prop_diabetes by dividing the number of people with Diabetes and Vgood health status by the total number of people with Diabetes
+            prop_noDiabetes = n[!hasDiabetes]/Total[!hasDiabetes], # Create a new column called prop_noDiabetes by dividing the number of people without Diabetes and with Vgood health status by the total number of people without Diabetes
+            diff_prop = prop_diabetes - prop_noDiabetes, # Create a new column called diff_prop by subtracting the proportion of people with Vgood health status and without Diabetes from the proportion of people with Vgood health status and Diabetes
+            # Create a new column called lower_ci by calling the prop.test function with arguments:
+            # a vector of two numbers: the number of people with Diabetes and Vgood health status, and the number of people without Diabetes and with Vgood health status,
+            # another vector of two numbers: the total number of people with Diabetes, and the total number of people without Diabetes,
+            # and the confidence level set to 95%
+            # The value stored in the column is the lower bound of the CI, extracted from the test result using $conf.int[1]
+            lower_ci = prop.test(c(n[hasDiabetes], n[!hasDiabetes]), c(Total[hasDiabetes], Total[!hasDiabetes]), conf.level = 0.95)$conf.int[1],
+            # Create a new column called upper_ci by calling the prop.test function with the same arguments as the line above
+            # This time the value stored in the column is the upper bound of the CI, extracted from the test result using $conf.int[2]
+            upper_ci = prop.test(c(n[hasDiabetes],n[!hasDiabetes]), c(Total[hasDiabetes],Total[!hasDiabetes]), conf.level = 0.95)$conf.int[2])
 
 
 

40c53445e113aab304927a3a7e6aa2ab60453c4c
Benjamin Gross <ben@benbox.org>
2024-02-07 12:57:47 +0100

    added some code

--- r-scripts/Week 5.R
+++ r-scripts/Week 5.R
@@ -3,3 +3,10 @@
 ## Libraries
 pacman::p_load(tidyverse, NHANES, here, ggplot2, cowplot)
 
+browseURL(here("meta_data", "Codebook for Apo E data.pdf"))
+
+browseURL(here("meta_data", "Codebook for LowBirthWt13 data.pdf"))
+
+read_csv(here("raw_data", "ApoE Study.csv"))
+
+read_csv(here("raw_data", "LowBirthWt13.csv"))

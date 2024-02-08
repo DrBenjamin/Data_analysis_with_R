75bd745854d4aa036ba3785c0be93d58cd49ee1d
Benjamin Gross <ben@benbox.org>
2024-02-08 11:35:16 +0100

    minor improvements

--- r-scripts/Week 5_Demo script.R
+++ r-scripts/Week 5_Demo script.R
@@ -15,10 +15,13 @@ browseURL(here("meta_data", "Extending Logistic Regression Lecture (slides).pdf"
 
 
 #### Example of standard logistic regression
+# Reading codebook
 browseURL(here("meta_data", "Codebook for Liver data.pdf"))
+
+# Loading data
 Liver <- read_csv(here("raw_data", "Liver.csv"))
 
-# Set up outcome variable as factor, with labelling from codebook.
+# Set up outcome variable as factor, with labelling from codebook
 Liver <- Liver %>%
   mutate(outcome = factor(outcome, labels = c("Alive", "Dead")))
 
@@ -43,12 +46,14 @@ anova(Liver.lr, test = "Chisq")
 
 
 #### Example of conditional logistic regression, with 1:1 matching
+# Reading codebook
 browseURL(here("meta_data", "Codebook for LowBirthWt data.pdf"))
+
+# Loading data
 LBW <- read_csv(here("raw_data", "LowBirthWt.csv"))
 
 # Unlike other functions, we need to keep the outcome variable as numeric here (don't change it to a factor). clogit needs
 # the numeric codes internally to work properly. Can & should transform the other variables to factors though...
-
 LBW <- LBW %>%
   mutate(
     smoke = factor(smoke),
@@ -66,7 +71,6 @@ LBW
 # is defined just as for a standard logistic model (response is
 # just case/control status) but with a strata() term to include
 # the matching variable.
-
 # Seeing model output, parameter estimates, ORs etc
 LBW.clr <- clogit(low ~ smoke + ht + ptd + strata(pair), data = LBW)
 summary(LBW.clr)
